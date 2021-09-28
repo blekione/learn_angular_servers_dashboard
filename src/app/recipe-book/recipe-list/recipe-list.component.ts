302e33b55a20cd4c6acee5e333dd89a6afa7fb54
Marcin Kruglik <blekione@gmail.comm>
2021-09-28 15:23:38 +0100

    Creates layouts of components.

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../model/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Schabowy z ziemniaczkami i sorowka', 
+      'Yummy Polish delicates', 
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Kotlet_Schabowy.jpg/540px-Kotlet_Schabowy.jpg'),
+    new Recipe(
+      'Bigos', 
+      'Kapusta w garku a mieso w lesie', 
+      'https://upload.wikimedia.org/wikipedia/commons/8/8b/Bigos_in_Krak%C3%B3w.jpg')
+  ];
 
   constructor() { }
 
